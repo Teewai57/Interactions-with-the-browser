fb780c036ac317d3737ada126430ac19af5a2d01
Teewai57 <bobtoyoabasi@gmail.com>
2022-03-28 09:54:14 +0100

    added classlists,innerhtml and styles

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
-// const mssg = document.querySelector('p')
-// mssg.setAttribute('style','color:red;')
-// mssg.innerHTML += '<h1>A new error</h1>';
-// mssg.style.fontSize = '20px';
-// mssg.style.margin = '50px';
+const mssg = document.querySelector('p')
+mssg.setAttribute('style','color:red;')
+mssg.innerHTML += '<h1>A new error</h1>';
+mssg.style.fontSize = '20px';
+mssg.style.margin = '50px';
 
 
 // CHANGE CLASSES USING JAVASCRIPT
 
-// const content = document.querySelector('p');
+const content = document.querySelector('p');
 
-// console.log(content.classList);
-// content.classList.add('change');
-// content.classList.remove('change')
+console.log(content.classList);
+content.classList.add('change');
+content.classList.remove('change')
 
-const paras = document.querySelectorAll('p');
+//  const paras = document.querySelectorAll('p');
 
-paras.forEach(p => {
-    if(p.textContent.includes('error')){
-        p.classList.add('error');
-    }
-    if(p.innerText.includes('success')){
-        p.classList.add('success')
-    }
-})
+// paras.forEach(p => {
+//     if(p.textContent.includes('error')){
+//         p.classList.add('error');
+//     }
+//     if(p.innerText.includes('success')){
+//         p.classList.add('success')
+//     }
+// })
 
-// TOGGLING CLASSES
+// // TOGGLING CLASSES
 
-const title = document.querySelector('.title');
+// const title = document.querySelector('.title');
 
-title.classList.toggle('test');
+// title.classList.toggle('test');
